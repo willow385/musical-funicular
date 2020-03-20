bd5f34f90cc7be17058a5bb87510efebbedf645a
Dante Falzone <dante.j.falzone@protonmail.com>
2020-03-20 17:08:53 -0500

    Still can't get the damn Scanner to work

--- src/main/java/mfun/Main.java
+++ src/main/java/mfun/Main.java
@@ -1,13 +1,20 @@
 package mfun;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("foo");
         Board board = new Board();
-        System.out.printf(board.toString());
-        while (true) {
-            board.refreshFrame();
+        Scanner sc = new Scanner(System.in);
+        do {
             System.out.printf(board.toString());
-        }
+            String input = "end";
+            if (input.equals("end")) {
+                break;
+            } else {
+                board.refreshFrame();
+            }
+        } while (sc.hasNext());
+
     }
 }
